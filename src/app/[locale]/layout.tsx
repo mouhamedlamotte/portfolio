0aa202d15fd103e8ccbfb13f28b4894fd4265baf
mouhamedlamotte <mouhamedlamotte.dev@gmail.com>
2025-01-13 12:48:17 +0000

    fix : remove FloatingChatWidget from layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -138,7 +138,6 @@ export default async function RootLayout({
             </Provider>
           <Toaster />
           <ImgPreview />
-          <FloatingChatWidget />
       </body>
     </html>
   );
